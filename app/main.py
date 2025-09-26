5a13534aadf0c655341130d4f6479202e87eb83c
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-09-26 13:26:53 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
+import functools
 
 
 def cache(func: Callable) -> Callable:
     cache_store = {}
-
-    def inner(*args) -> None:
-        if args in cache_store:
-            result = cache_store[args]
+    @functools.wraps(func)
+    def inner(*args, **kwargs) -> Any:
+        sorted_kwargs = tuple(sorted(kwargs.items()))
+        key = (args, sorted_kwargs)
+        if key in cache_store:
+            result = cache_store[key]
             print("Getting from cache")
         else:
-            result = func(*args)
-            cache_store[args] = result
+            result = func(*args, **kwargs)
+            cache_store[key] = result
             print("Calculating new result")
         return result
     return inner
